eb85e811704dcf6c232b67dc9eaddadd458c2f88
sted9000 <sted9000@gmail.com>
2018-11-17 11:37:53 -0300

    working commit before changing test contract

--- js/web3_starter.js
+++ js/web3_starter.js
@@ -1,26 +1,69 @@
-// Find connection; else throw error
-window.addEventListener('load', function() {
+window.addEventListener('load', async () => {
 
-  // First
-  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-  if (typeof web3 === 'undefined') {
+    // First see you browser has a web3 injection
+    if (typeof web3 === 'undefined') {
 
-    // Throw error
-    $('body').addClass('error-no-metamask-plugin').addClass('error');
+        // If not throw error
+        $('body').addClass('error-no-metamask-plugin').addClass('error');
 
-  } else {
-      // Use Mist/MetaMask's provider
-      console.log('Web3 provider found');
-      web3 = new Web3(web3.currentProvider);
-  }
+    // If yes get the account access
+    } else {
+        // *** Pattern for sites which need account address access **************
+        // Modern dapp browsers...
+        if (window.ethereum) {
 
-    // Second
+            window.web3 = new Web3(ethereum);
+
+            try {
+                // Request account access if needed
+                await ethereum.enable();
+                var accountAddressNeeded = true;
+                checkNetwork(accountAddressNeeded);
+
+            } catch (error) {
+                // throw err message
+                $('body').addClass('error-no-account-access').addClass('error');
+            }
+        // Legacy dapp browsers...
+        } else if (window.web3) {
+            window.web3 = new Web3(web3.currentProvider);
+
+            // Call network check
+            var accountAddressNeeded = true;
+            checkNetwork(accountAddressNeeded);
+        }
+        //*************************************************************************
+
+        // *** Pattern for stites which do NOT need account address access ********
+        // if (window.ethereum) {
+        //     window.web3 = new Web3(ethereum);
+        //     var accountAddressNeeded = false
+        //     checkNetwork(accountAddressNeeded);
+        // }
+        // else if (window.web3) {
+        //     window.web3 = new Web3(web3.currentProvider);
+        //     checkNetwork(accountAddressNeeded);
+        // }
+
+        //**************************************************************************
+
+        // Non-dapp browsers... (no metamask plugin)
+        else {
+            $('body').addClass('error-no-metamask-plugin').addClass('error');
+        }
+    }
+});
+
+function checkNetwork(_accountAddressNeeded) {
+
     // Network check; else throw error
     web3.version.getNetwork((err, netId) => {
 
       switch (netId) {
         case "1":
           console.log('This is mainnet');
+          // Case account check
+          // accountCheck(_accountAddressNeeded);
           $('body').addClass('error-invalid-network').addClass('error');
           break
         case "2":
@@ -29,7 +72,8 @@ window.addEventListener('load', function() {
           break
         case "3":
           console.log('This is the ropsten test network.');
-          // Not throw error message and screen
+          accountCheck(_accountAddressNeeded);
+          // $('body').addClass('error-invalid-network').addClass('error');
           break
         case "4":
           console.log('This is the Rinkeby test network.');
@@ -43,49 +87,70 @@ window.addEventListener('load', function() {
           console.log('This is an unknown network.');
           $('body').addClass('error-invalid-network').addClass('error');
       }
+  });
+}
 
-       // Third
-       // User has an account
-       web3.eth.getAccounts((err, acc) => {
-           if (!err) {
+function accountCheck(_accountAddressNeeded) {
+    window.web3.eth.getAccounts((err, acc) => {
 
-               if (acc.length <= 0) {
-                   $('body').addClass('error-no-metamask-accounts').addClass('error');
-               } else {
-                   web3.eth.defaultAccount = web3.eth.accounts[0];
-                   console.log("Active account: " + web3.eth.defaultAccount);
+        if (!err) {
+            console.log('here in accountcheck');
+            console.log('accountAddressNeeded: ' + _accountAddressNeeded);
 
-                   // Fire first function
-                   newSearch();
-               }
+            if (_accountAddressNeeded) {
 
-         } else {
-             console.error(err);
-         }
-     });
-    });
-});
+                if (acc.length <= 0) {
+                    $('body').addClass('error-no-metamask-accounts').addClass('error');
+
+                } else {
+                    web3.eth.defaultAccount = web3.eth.accounts[0];
+                    console.log("Active account: " + web3.eth.defaultAccount);
+                    // Call set abi
+                    fireUponPageLoad();
+                }
+            }
+            else {
+                console.log('No account address needed');
+                fireUponPageLoad();
+            }
+
+       } else {
+          // Terribly wrong error message
+       }
+   });
+}
+
+
+
 
 // web3 opjects (factoryInstance and freerollABI)
-  var factoryABI = web3.eth.contract([
+var factoryABI = web3.eth.contract([
 	{
 		"constant": false,
 		"inputs": [
-			{
-				"name": "_first_receiver",
-				"type": "address"
-			},
-			{
-				"name": "_second_receiver",
-				"type": "address"
-			},
 			{
 				"name": "_descr",
 				"type": "string"
 			},
 			{
+				"name": "_receiver",
+				"type": "address"
+			},
+			{
+				"name": "_charity",
+				"type": "bool"
+			},
+			{
+				"name": "_category",
+				"type": "uint256"
+			},
+			{
 				"name": "_duration",
 				"type": "uint256"
+			},
+			{
+				"name": "_location",
+				"type": "string"
 			}
 		],
 		"name": "newFreeroll",
@@ -100,121 +165,79 @@ window.addEventListener('load', function() {
 		"type": "function"
 	},
 	{
+		"constant": true,
 		"inputs": [],
+		"name": "version",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"name": "_version",
+				"type": "uint256"
+			}
+		],
 		"payable": false,
 		"stateMutability": "nonpayable",
 		"type": "constructor"
 	},
 	{
-		"constant": true,
+		"anonymous": false,
 		"inputs": [
 			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "communityFreerolls",
-		"outputs": [
-			{
-				"name": "",
-				"type": "address"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "lenCommunityFreerolls",
-		"outputs": [
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "_user_address",
-				"type": "address"
-			}
-		],
-		"name": "lenUserFreerolls",
-		"outputs": [
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "",
+				"indexed": true,
+				"name": "_poster",
 				"type": "address"
 			},
 			{
-				"name": "",
+				"indexed": false,
+				"name": "_description",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "_value",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"name": "_charity",
+				"type": "bool"
+			},
+			{
+				"indexed": false,
+				"name": "_category",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"name": "_location",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "_duration",
 				"type": "uint256"
 			}
 		],
-		"name": "userFreerolls",
-		"outputs": [
-			{
-				"name": "",
-				"type": "address"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
+		"name": "FreerollLog",
+		"type": "event"
 	}
 ]);
 
-var factoryAddress = '0xa59136ca4f152e3c896da8c9aeda07c0ef72b3d3'; // Ropstein Testnet Factory Address
+var factoryAddress = '0x7949088B58158C631aAf341C677777D214589CC4'; // Ropstein Testnet Factory Address
 
 var factoryInstance = factoryABI.at(factoryAddress); // Connecting Factory ABI and Address
 
 var freerollABI = web3.eth.contract([
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "first_receiver",
-		"outputs": [
-			{
-				"name": "",
-				"type": "address"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "rebate_contstant",
-		"outputs": [
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
 	{
 		"constant": true,
 		"inputs": [],
@@ -223,20 +246,6 @@ var freerollABI = web3.eth.contract([
 			{
 				"name": "",
 				"type": "uint256"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "second_receiver",
-		"outputs": [
-			{
-				"name": "",
-				"type": "address"
 			}
 		],
 		"payable": false,
@@ -267,20 +276,6 @@ var freerollABI = web3.eth.contract([
 		"type": "function"
 	},
 	{
-		"constant": true,
-		"inputs": [],
-		"name": "descr",
-		"outputs": [
-			{
-				"name": "",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
 		"constant": false,
 		"inputs": [],
 		"name": "posterLost",
@@ -290,22 +285,28 @@ var freerollABI = web3.eth.contract([
 		"type": "function"
 	},
 	{
+		"constant": true,
+		"inputs": [],
+		"name": "receiver",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
 		"inputs": [
 			{
 				"name": "_poster",
 				"type": "address"
 			},
 			{
-				"name": "_first_receiver",
+				"name": "_receiver",
 				"type": "address"
-			},
-			{
-				"name": "_second_receiver",
-				"type": "address"
-			},
-			{
-				"name": "_descr",
-				"type": "string"
 			},
 			{
 				"name": "_duration",
---
+++ solidity/Factory.sol
@@ -0,0 +1,336 @@
+pragma solidity ^0.4.25;
+
+/*
+* Version 0.1
+*/
+
+library SafeMath {
+  function mul(uint256 _a, uint256 _b) internal pure returns (uint256 c) {
+    if (_a == 0) {
+      return 0;
+    }
+
+    c = _a * _b;
+    assert(c / _a == _b);
+    return c;
+  }
+
+  function div(uint256 _a, uint256 _b) internal pure returns (uint256) {
+    return _a / _b;
+  }
+
+  function sub(uint256 _a, uint256 _b) internal pure returns (uint256) {
+    assert(_b <= _a);
+    return _a - _b;
+  }
+
+  function add(uint256 _a, uint256 _b) internal pure returns (uint256 c) {
+    c = _a + _b;
+    assert(c >= _a);
+    return c;
+  }
+} // End of SafeMath library
+
+
+contract Factory {
+
+	uint256 public version; // Version of the Factory contract
+
+    event FreerollLog (
+        address indexed _poster,
+        string _description,
+        uint256 _value,
+        bool _charity,
+        uint256 _category,
+        string _location,
+        uint256 _duration
+        );
+
+	constructor(uint256 _version) public {
+		version = _version;
+	}
+
+	// Create new freeroll
+	function newFreeroll(string _descr,
+        address _receiver,
+        bool _charity,
+        uint256 _category,
+		uint256 _duration,
+        string _location)
+		public payable returns(address) {
+
+		Freeroll c = (new Freeroll).value(msg.value)(msg.sender, // Deploy a new Freeroll contract (with msg.value)
+            _receiver,
+			_duration
+            );
+
+        emit FreerollLog( // Emit a log
+            msg.sender,
+            _descr,
+            msg.value,
+            _charity,
+            _category,
+            _location,
+            _duration
+            );
+
+		return c; // Returns new Freeroll contract address
+	}
+} // End of Factory contract
+
+
+contract Freeroll {
+
+	using SafeMath for uint256;
+
+	address public poster; // Freeroll poster
+	address public receiver; // Address to receive funds if poster losses
+	uint256 public end_date; // Wager deadline (unix time)
+
+	constructor(address _poster, address _receiver, uint256 _duration) payable public {
+		poster = _poster;
+		receiver = _receiver;
+		end_date = _duration.add(block.timestamp);
+	}
+
+	function() public payable {} // Contract can accept ETH
+
+	// Poster completes goal
+	function posterWon() public onlyPoster {
+		require(block.timestamp <= end_date); // The freeroll's end_date has not passed
+		require(msg.sender.send(address(this).balance)); // Refund the poster his ETH
+	}
+
+	// Poster fails -  public call to distribute funds
+	function posterLost() public {
+		require(block.timestamp > end_date);
+		require(receiver.send((address(this).balance).sub(1))); // 100% (minus 1wei) to receiver
+	}
+
+	// Modifiers
+	modifier onlyPoster {
+		require(msg.sender == poster);
+			_;
+	}
+} // End of Freeroll contract
+
+/* Data:
+- Network: Ropsten
+- Version: 0;
+- Gas used to deplay factory: 629168;
+- Address: 0x7949088B58158C631aAf341C677777D214589CC4;
+
+First Freeroll:
+"The first freeroll on the ropsten test network Nov 14th", "0xAa6d662B5B7709F1A55F29A8FAEee549D0ad37ef", false, 0, 60, "tedslocum.com"
+Amount: .02 or .002 I forget
+- Gas used: 309193;
+- About 6cent per gwei
+- Address: 0xB64ECB450eF49287d67384A2Ca9f1FdF2944b683;
+
+Second freeroll: (long duration, charity, category 1,):
+"The second freeroll (long duration and charity)", "0xAa6d662B5B7709F1A55F29A8FAEee549D0ad37ef", true, 1, 31536000, "jonnyspicer.com"
+Amount: .03
+- Gas used: 309129
+
+
+
+
+Factory abi:
+[
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_descr",
+				"type": "string"
+			},
+			{
+				"name": "_receiver",
+				"type": "address"
+			},
+			{
+				"name": "_charity",
+				"type": "bool"
+			},
+			{
+				"name": "_category",
+				"type": "uint256"
+			},
+			{
+				"name": "_duration",
+				"type": "uint256"
+			},
+			{
+				"name": "_location",
+				"type": "string"
+			}
+		],
+		"name": "newFreeroll",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "version",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"name": "_version",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "_poster",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "_description",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "_value",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"name": "_charity",
+				"type": "bool"
+			},
+			{
+				"indexed": false,
+				"name": "_category",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"name": "_location",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "_duration",
+				"type": "uint256"
+			}
+		],
+		"name": "FreerollLog",
+		"type": "event"
+	}
+]
+
+Freeroll Abi:
+[
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "end_date",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "poster",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "posterWon",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "posterLost",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "receiver",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"name": "_poster",
+				"type": "address"
+			},
+			{
+				"name": "_receiver",
+				"type": "address"
+			},
+			{
+				"name": "_duration",
+				"type": "uint256"
+			}
+		],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "constructor"
+	},
+	{
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "fallback"
+	}
+]
+
+*/
